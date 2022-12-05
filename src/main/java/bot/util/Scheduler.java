06bced575a8382fd7a1f3e7ef8f7d9595950ce76
denys.kozii <denys.kozii@travisperkins.co.uk>
2022-12-05 18:15:43 +0200

    cut position timer

--- src/main/java/bot/util/Scheduler.java
+++ src/main/java/bot/util/Scheduler.java
@@ -64,7 +64,7 @@ public class Scheduler {
     }
 
     @SneakyThrows
-    @Scheduled(cron = "10 0 0 * * *", zone = "GMT+0")
+    @Scheduled(cron = "8 0 0 * * *", zone = "GMT+0")
     public void close0() {
         close(0);
     }
@@ -88,7 +88,7 @@ public class Scheduler {
     }
 
     @SneakyThrows
-    @Scheduled(cron = "0 0 8 * * *", zone = "GMT+0")
+    @Scheduled(cron = "8 0 8 * * *", zone = "GMT+0")
     public void open8() {
         open();
     }
@@ -118,7 +118,7 @@ public class Scheduler {
     }
 
     @SneakyThrows
-    @Scheduled(cron = "0 0 16 * * *", zone = "GMT+0")
+    @Scheduled(cron = "8 0 16 * * *", zone = "GMT+0")
     public void open16() {
         open();
     }
