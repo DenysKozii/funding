42b3c9fc8697697720c9d74415e278a793f1252c
den <denys.kozii@gmail.com>
2023-02-12 19:25:22 +0200

    decrease profit

--- src/main/java/bot/dto/ProfitLevel.java
+++ src/main/java/bot/dto/ProfitLevel.java
@@ -11,8 +11,8 @@ import lombok.experimental.FieldDefaults;
 public enum ProfitLevel {
     REJECT(0.0, 0.005),
     LOW(0.0009, 0.01),
-    MEDIUM(0.0019, 0.015),
-    HIGH(0.0029, 1.0);
+    MEDIUM(0.0017, 0.015),
+    HIGH(0.0025, 1.0);
 
     Double profit;
     Double funding;
