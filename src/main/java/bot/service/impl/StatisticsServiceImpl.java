cf853401d63d98c542265b890a233a11d6038c1a
den <denys.kozii@gmail.com>
2023-04-08 13:58:47 +0300

    add statistics summing

--- src/main/java/bot/service/impl/StatisticsServiceImpl.java
+++ src/main/java/bot/service/impl/StatisticsServiceImpl.java
@@ -1,24 +1,35 @@
 package bot.service.impl;
 
 import bot.dto.StatisticsDto;
+import bot.entity.Trade;
+import bot.repository.TradeRepository;
 import bot.service.StatisticsService;
 import lombok.AccessLevel;
-import lombok.Data;
+import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
-@Data
 @Slf4j
 @Service
+@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class StatisticsServiceImpl implements StatisticsService {
 
+    TradeRepository tradeRepository;
+
     @Override
     public List<StatisticsDto> getStatistics() {
-        return null;
+        ArrayList<StatisticsDto> statistics = new ArrayList<>();
+        Double profitSum = tradeRepository.findAll().stream()
+                .map(Trade::getProfit)
+                .mapToDouble(Double::doubleValue)
+                .sum();
+        statistics.add(new StatisticsDto("Denys", profitSum));
+        return statistics;
     }
 }
